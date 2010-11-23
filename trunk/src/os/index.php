0b1ca2a5e4195f0d65d555049cfa7f6d0a8e070f
ErikKinding <erikkinding@gmail.com>
2010-11-23 18:08:18 +0100

    Updated file encodings to UTF-8 instead of that Mac OS thing... Also, added a HTML heading with doctype and that stuff to index.php.

--- trunk/src/os/index.php
+++ trunk/src/os/index.php
@@ -1,4 +1,17 @@
 <?php
+
+	echo <<<EOF
+	<?xml version="1.0" encoding="UTF-8"?>
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+	<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+	<head>	
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" type="text/css" src="css/layout.css" />
+	</head>
+	<body>
+EOF;
+	
+
 	session_start();
 	require_once "db.php";
 	require_once "core.php";
@@ -16,4 +29,9 @@
 		require_once "ajax.php";
 	}
 	
+	
+	echo <<<EOF
+	</body>
+	</html>
+EOF;
 ?>
