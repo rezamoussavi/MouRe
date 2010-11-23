d70e4d615214061991d628cdffe53bfc94b53bcb
ErikKinding <erikkinding@gmail.com>
2010-11-23 18:42:26 +0100

    session line in index.php fixed

--- trunk/src/os/index.php
+++ trunk/src/os/index.php
@@ -1,5 +1,6 @@
 <?php
-
+	session_start();
+	
 	echo <<<EOF
 	<?xml version="1.0" encoding="UTF-8"?>
 	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
@@ -12,7 +13,6 @@
 EOF;
 	
 
-	session_start();
 	require_once "db.php";
 	require_once "core.php";
 	require_once "bizbank.php"; //make a new object from propper bizbank class and set into $bizbank
