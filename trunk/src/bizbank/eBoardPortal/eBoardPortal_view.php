a67f7be767ad2f136555bddee587ab7fcf10191f
ErikKinding <erikkinding@gmail.com>
2010-11-26 10:25:36 +0100

    changed encoding of bizbank files to utf-8

--- trunk/src/bizbank/eBoardPortal/eBoardPortal_view.php
+++ trunk/src/bizbank/eBoardPortal/eBoardPortal_view.php
@@ -1,20 +1,21 @@
 <body>
- <div id="contentholder">
-     <div class="content">
-     <div class="toprow">
-         <div class="login">
+<div id="contentholder">
+    <div class="content">
+    <div class="toprow">
+        <div class="login">
+        
+        <?php $this->login->show() ?>
          
-         <?php $this->login->show() ?>
-         
-         </div>
+        </div>
  
-         <div class="banner">
-             <h1>A Board Name</h1>
-         </div>
+        <div class="banner">
+            <h1>A Board Name</h1>
+        </div>
  
-         <div class="helpinfo"> hej 3
-         </div>
-     </div>
-     </div>
- </div>        
- </body>
+        <div class="helpinfo">
+            <h2>info here?</h2>
+        </div>
+    </div>
+    </div>
+</div>        
+</body>
