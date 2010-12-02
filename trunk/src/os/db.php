85f754c5c77b0bea1cd31d601333cf3465d0a797
rezamoussavi <reza2mussavi@hotmail.com>
2010-12-02 19:59:19 +0100

    xquery function added to os to suport "select * from #db->info" instead of "select * from user_info"

--- trunk/src/os/db.php
+++ trunk/src/os/db.php
@@ -15,6 +15,21 @@
 		echo("can not open DB");
 	}
 	$_result=null;
+	function parseQuery($caller,$s){
+		//simple replaces
+		$cmd=array("#db->","#dbfilter->","#by_bizness");
+		$sql=array(get_class($caller)."_","(SELECT * FROM ","WHERE biznessUID=".osBackBizness().")");
+		$s=str_replace($cmd,$sql,$s);
+		//functional replaces
+		
+		//finally
+		return $s;
+	}
+	function xquery($caller,$s){
+		global $osdbcon,$_result;
+		if(!$osdbcon){ $_result=null; return;}
+		$_result= mysql_query(parseQuery($caller,$s),$osdbcon);
+	}
 	function query($s){
 		global $osdbcon,$_result;
 		if(!$osdbcon){ $_result=null; return;}
