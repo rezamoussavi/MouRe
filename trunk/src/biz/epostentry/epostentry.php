fe9221ac73f342c967e0287ca601ee0ce63f52fb
rezamoussavi <reza2mussavi@hotmail.com>
2011-01-03 19:43:04 +0100

    bizLang description added + some new bizes

---
+++ trunk/src/biz/epostentry/epostentry.php
@@ -0,0 +1,80 @@
+<?PHP
+
+/*
+	Compiled by bizLang compiler version 1.0
+
+	Author:		Reza Moussavi
+	Version:	0.1
+
+*/
+
+class epostentry {
+
+	//Mandatory Variables for a biz
+	var $_bizname;
+	var $_fullname;
+	var $_parent;
+	var $_curFrame;
+
+	//Variables
+
+	//Nodes (bizvars)
+
+	function __construct(&$data) {
+		if (!isset($data['sleep'])) {
+			$data['sleep'] = true;
+			$this->_initialize($data);
+			$this->_wakeup($data);
+		}else{
+			$this->_wakeup($data);
+		}
+	}
+
+	function _initialize(&$data){
+	}
+
+	function _wakeup(&$data){
+		$this->_bizname = &$data['bizname'];
+		$this->_fullname = &$data['fullname'];
+		$this->_parent = &$data['parent'];
+		$this->_curFrame = &$data['curFrame'];
+
+
+	}
+
+	function message($to, $message, $info) {
+		if ($to != $this->_fullname) {
+			return;
+		}
+		switch($message){
+			default:
+				break;
+		}
+	}
+
+	function broadcast($message, $info) {
+		switch($message){
+			default:
+				break;
+		}
+	}
+
+	function _bookframe($frame){
+		$this->_curFrame=$frame;
+		$this->show(true);
+	}
+	function _backframe(){
+		return $this->show(false);
+	}
+
+	function show($echo){
+		$html='<div id="' . $this->_fullname . '">'.call_user_func(array($this, $this->_curFrame)).'</div>';
+		if($echo)
+			echo $html;
+		else
+			return $html;
+	}
+
+}
+
+?>
