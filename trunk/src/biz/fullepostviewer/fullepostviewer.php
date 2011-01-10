4823c292831b23b92d2f91741eb5c7624b814c97
rezamoussavi <reza2mussavi@hotmail.com>
2011-01-10 19:31:45 +0100

    some more ebFrame related bizes added

--- trunk/src/biz/fullepostviewer/fullepostviewer.php
+++ trunk/src/biz/fullepostviewer/fullepostviewer.php
@@ -1,14 +1,17 @@
 <?PHP
 
 /*
-	Compiled by bizLang compiler version 1.0
+	Compiled by bizLang compiler version 1.01
 
 	Author:		Reza Moussavi
 	Version:	0.1
+	Date:		1/10/2011
+	TestApproval: none
 
 */
+require_once '../biz/epostviewer/epostviewer.php';
 
-class postbank {
+class fullepostviewer {
 
 	//Mandatory Variables for a biz
 	var $_bizname;
@@ -17,20 +20,40 @@ class postbank {
 	var $_curFrame;
 
 	//Variables
+	var $expanded;
+	var $UID;
+	var $isLoaded;
 
 	//Nodes (bizvars)
+	var $post;
+	var $comments_array_data; 	var $comments;
 
 	function __construct(&$data) {
 		if (!isset($data['sleep'])) {
 			$data['sleep'] = true;
 			$this->_initialize($data);
 			$this->_wakeup($data);
+			$this->init(); //Customized Initializing
 		}else{
 			$this->_wakeup($data);
 		}
 	}
 
 	function _initialize(&$data){
+		if(! isset ($data['curFrame']))
+			$data['curFrame']=frm;
+		if(! isset ($data['expanded']))
+			$data['expanded']=false;
+		if(! isset ($data['UID']))
+			$data['UID']=-1;
+		if(! isset ($data['isLoaded']))
+			$data['isLoaded']=false;;
+		if(! isset ($data['post'])){
+			$data['post']['fullname']=$this->_fullname.'_post';
+			$data['post']['bizname']='post';
+		}
+		if(! isset ($data['comments_array_data']))
+			$data['comments_array_data']=array();
 	}
 
 	function _wakeup(&$data){
@@ -39,11 +62,30 @@ class postbank {
 		$this->_parent = &$data['parent'];
 		$this->_curFrame = &$data['curFrame'];
 
+		$this->expanded=&$data['expanded'];
+		$this->UID=&$data['UID'];
+		$this->isLoaded=&$data['isLoaded'];
 
+		$data['post']['parent']=$this;
+		$this->post=new epostviewer($data['post']);
+
+		$this->comments=array();
+		$this->comments_array_data=&$data['comments_array_data'];
+		foreach($data['comments_array_data'] as $na=>&$da){
+			if(! isset($da['bizname'])){
+				$da['bizname']=$na;
+				$da['fullname']=$this->_fullname."_".$na;
+				$da['parent']=$this;
+			}
+			$this->comments[]=new fullepostviewer($da);
+		}
 	}
 
 	function message($to, $message, $info) {
 		if ($to != $this->_fullname) {
+			$this->post->message($to, $message, $info);
+			foreach($this->comments as $i=>&$_element)
+				$_element->message($to, $message, $info);
 			return;
 		}
 		switch($message){
@@ -53,6 +95,9 @@ class postbank {
 	}
 
 	function broadcast($message, $info) {
+		$this->post->broadcast($message, $info);
+		foreach($this->comments as $i=>&$_element)
+			$_element->broadcast($message, $info);
 		switch($message){
 			default:
 				break;
@@ -73,6 +118,64 @@ class postbank {
 			echo $html;
 		else
 			return $html;
+	}
+
+
+//########################################
+//         YOUR FUNCTIONS GOES HERE
+//########################################
+
+
+	function bookUID($UID){
+		$this->post->bookUID($this->UID);
+	}
+	function bookExpanded($expanded){
+		if(! $this->isLoaded && $expanded){
+			$this->loadComments();
+		}
+		$this->expanded=$expanded;
+	}
+	function loadComments(){
+		$comments=$this->post->backCommentsUID();
+		
+		// Empty the array
+		$this->comments_array_data=array();
+		$this->comments=array();
+		foreach($comments as $c){
+			
+			// Add new Node to the array
+			$_index=count($this->comments_array_data);
+			$_data=array();
+			$_data['parent']=$this;
+			$_data['bizname']=$_index;
+			$_data['fullname']=$this->_fullname.'_'.$_index;
+			$this->comments_array_data[]=$_data;
+			$this->comments[]=new  fullepostviewer($this->comments_array_data[$_index]);
+			end($this->comments)->bookUID($c);
+		}
+		$this->isLoaded=true;
+	}
+	function onExpand($info){
+		$this->bookExpanded(!$this->expanded);
+	}
+	function frm(){
+		$post=$this->post->_backframe();
+		$this->expanded? $sign='[-]' : $sign='[+]' ;
+		$comments=' ';
+		if($this->expanded){
+			foreach($this->comments as c){
+				$comments.=c->_backframe();
+			}
+		}
+		$html=<<<PHTML
+			$post<br>
+			<form name="{$this->_fullname}" method="post">
+				<input type="hidden" name="_message" value="expand" /><input type = "hidden" name="_target" value="' . $this->_fullname . '" />
+				<input value ="$sign" type = "button" onclick = 'JavaScript:sndmsg("{$this->_fullname}")' />
+			</form><br>
+			$comments<br>
+PHTML;
+		return $html;
 	}
 
 }
