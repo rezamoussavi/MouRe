982cadf7f9d47ddd0845e5b04506df8bc0fe59d1
rezamoussavi <reza2mussavi@hotmail.com>
2011-02-14 18:34:35 +0100

    biz update on KOPON

---
+++ trunk/kopon/biz/mainviewer/mainviewer.php
@@ -0,0 +1,144 @@
+<?PHP
+
+/*
+	Compiled by bizLang compiler version 1.4 (Feb 4 2011) By Reza Moussavi
+	1.1: {Family included}
+	1.2: {flatten sleep session}
+	1.3: {direct message sending}
+	1.3.5: {sleep and decunstructed merged + _tmpNode_ added to fix a bug}
+	1.4: {multi parameter in link message}
+
+	Author: Reza Moussavi
+	Date:	02/10/2011
+	Version: 0.2
+	---------------------
+	Author: Reza Moussavi
+	Date:	02/07/2011
+	Version: 0.1
+
+*/
+require_once '../biz/tabbank/tabbank.php';
+require_once '../biz/multipageviewer/multipageviewer.php';
+
+class mainviewer {
+
+	//Mandatory Variables for a biz
+	var $_fullname;
+	var $_curFrame;
+	var $_tmpNode;
+
+	//Variables
+
+	//Nodes (bizvars)
+	var $tabbar;
+	var $pages;
+
+	function __construct($fullname) {
+		$this->_tmpNode=false;
+		if($fullname==null){
+			$fullname='_tmpNode_'.count($_SESSION['osNodes']);
+			$this->_tmpNode=true;
+		}
+		$this->_fullname=$fullname;
+		if(!isset($_SESSION['osNodes'][$fullname])){
+			$_SESSION['osNodes'][$fullname]=array();
+			//If any message need to be registered will placed here
+			$_SESSION['osMsg']['user_logedin'][$this->_fullname]=true;
+			$_SESSION['osMsg']['user_logedout'][$this->_fullname]=true;
+		}
+
+		//default frame if exists
+		if(!isset($_SESSION['osNodes'][$fullname]['_curFrame']))
+			$_SESSION['osNodes'][$fullname]['_curFrame']='frm';
+		$this->_curFrame=&$_SESSION['osNodes'][$fullname]['_curFrame'];
+
+		$this->tabbar=new tabbank($this->_fullname.'_tabbar');
+
+		$this->pages=new multipageviewer($this->_fullname.'_pages');
+
+		$this->init(); //Customized Initializing
+		$_SESSION['osNodes'][$fullname]['node']=$this;
+		$_SESSION['osNodes'][$fullname]['biz']='mainviewer';
+	}
+
+	function sleep(){
+		$_SESSION['osNodes'][$this->_fullname]['slept']=true;
+	}
+
+	function __destruct() {
+		if($this->_tmpNode)
+			unset($_SESSION['osNodes'][$this->_fullname]);
+		else
+			unset($_SESSION['osNodes'][$this->_fullname]['node']);
+	}
+
+
+	function message($message, $info) {
+		switch($message){
+			case 'user_logedin':
+				$this->onLogedin($info);
+				break;
+			case 'user_logedout':
+				$this->onLogedout($info);
+				break;
+			default:
+				break;
+		}
+	}
+
+	function _bookframe($frame){
+		$this->_curFrame=$frame;
+		$this->show(true);
+	}
+	function _backframe(){
+		return $this->show(false);
+	}
+
+	function show($echo){
+		$html='<div id="' . $this->_fullname . '">'.call_user_func(array($this, $this->_curFrame)).'</div>';
+		if($_SESSION['silentmode'])
+			return;
+		if($echo)
+			echo $html;
+		else
+			return $html;
+	}
+
+
+//########################################
+//         YOUR FUNCTIONS GOES HERE
+//########################################
+
+
+	function init(){
+		$tab=array("Main","Previous","How");
+		if(osIsAdmin()){
+			$tab[]="CPanel";
+		}
+		if(osBackUser()!=-1){
+			$tab[]="MyAcc";
+		}
+		$this->tabbar->bookContent($tab);
+	}
+	function onLogedin($info){
+		$this->init();
+		_bookFrame("frm");
+	}
+	function onLogedout($info){
+		$this->init();
+		_bookFrame("frm");
+	}
+	function frm(){
+		$tab=$this->tabbar->_backFrame();
+		$pages=$this->pages->_backFrame();
+		$html=<<<PHTMLCODE
+
+			$tab <br> $pages
+		<<<PHTMLCODE
+;
+		return $html;
+	}
+
+}
+
+?>
---
+++ trunk/kopon/biz/tab/tab.php
@@ -0,0 +1,94 @@
+<?PHP
+
+/*
+	Compiled by bizLang compiler version 1.4 (Feb 4 2011) By Reza Moussavi
+	1.1: {Family included}
+	1.2: {flatten sleep session}
+	1.3: {direct message sending}
+	1.3.5: {sleep and decunstructed merged + _tmpNode_ added to fix a bug}
+	1.4: {multi parameter in link message}
+
+	Author: Reza Moussavi
+	Date:	02/07/2011
+	Version: 0.1
+
+*/
+
+class tab {
+
+	//Mandatory Variables for a biz
+	var $_fullname;
+	var $_curFrame;
+	var $_tmpNode;
+
+	//Variables
+
+	//Nodes (bizvars)
+
+	function __construct($fullname) {
+		$this->_tmpNode=false;
+		if($fullname==null){
+			$fullname='_tmpNode_'.count($_SESSION['osNodes']);
+			$this->_tmpNode=true;
+		}
+		$this->_fullname=$fullname;
+		if(!isset($_SESSION['osNodes'][$fullname])){
+			$_SESSION['osNodes'][$fullname]=array();
+			//If any message need to be registered will placed here
+		}
+
+		$_SESSION['osNodes'][$fullname]['node']=$this;
+		$_SESSION['osNodes'][$fullname]['biz']='tab';
+	}
+
+	function sleep(){
+		$_SESSION['osNodes'][$this->_fullname]['slept']=true;
+	}
+
+	function __destruct() {
+		if($this->_tmpNode)
+			unset($_SESSION['osNodes'][$this->_fullname]);
+		else
+			unset($_SESSION['osNodes'][$this->_fullname]['node']);
+	}
+
+
+	function message($message, $info) {
+		switch($message){
+			default:
+				break;
+		}
+	}
+
+	function _bookframe($frame){
+		$this->_curFrame=$frame;
+		$this->show(true);
+	}
+	function _backframe(){
+		return $this->show(false);
+	}
+
+	function show($echo){
+		$html='<div id="' . $this->_fullname . '">'.call_user_func(array($this, $this->_curFrame)).'</div>';
+		if($_SESSION['silentmode'])
+			return;
+		if($echo)
+			echo $html;
+		else
+			return $html;
+	}
+
+
+//########################################
+//         YOUR FUNCTIONS GOES HERE
+//########################################
+
+
+	function bookLabel($label){//String
+	}
+	function bookSelected($selected){//boolean
+	}
+
+}
+
+?>
