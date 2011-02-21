52cefb003c286dcfd597090dc002642284ab0698
rezamoussavi <reza2mussavi@hotmail.com>
2011-02-21 21:46:26 +0100

    compiler 1.5 multi secName (short and complete name)

--- trunk/compiler/PHPClass.java
+++ trunk/compiler/PHPClass.java
@@ -51,12 +51,13 @@ public class PHPClass {
 		}else if(sec.name.equalsIgnoreCase("phpfunction")){
 			functions=sec.elements.get(0).data;
 		}else if(sec.name.equalsIgnoreCase("comments")){
-			comments= "/*\n\tCompiled by bizLang compiler version 1.4 (Feb 4 2011) By Reza Moussavi\n" +
+			comments= "/*\n\tCompiled by bizLang compiler version 1.5 (Feb 21 2011) By Reza Moussavi\n" +
 			"\t1.1: {Family included}\n" +
 			"\t1.2: {flatten sleep session}\n" +
 			"\t1.3: {direct message sending}\n" +
 			"\t1.3.5: {sleep and decunstructed merged + _tmpNode_ added to fix a bug}\n" +
-			"\t1.4: {multi parameter in link message}\n\n" +
+			"\t1.4: {multi parameter in link message}\n" +
+			"\t1.5: {multi secName support: frm/frame, msg/messages,fun/function/phpfunction}\n\n" +
 			sec.elements.get(0).data+"\n*/\n";
 		}
 	}
