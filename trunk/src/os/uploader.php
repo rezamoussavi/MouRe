4663135cfb76781f39150b5af1bf55ecdb2269b3
rezamoussavi <reza2mussavi@hotmail.com>
2011-03-22 20:54:14 +0100

    uploader upgraded in OS

--- trunk/src/os/uploader.php
+++ trunk/src/os/uploader.php
@@ -1,9 +1,18 @@
 <?php
 	if ($_FILES["file"]["error"] > 0){
+		//##################################//File has been uploaded with errors
 		echo "Error: " . $_FILES["file"]["error"] . "<br />";
-	}else
-	{
-		echo "ok";
+	}else{
+		//##################################// Files has been uploaded correctly
+		if(!is_dir("biz/".$_POST['biz']."/")){
+			//##################################// Creat biz directory if not exists
+			mkdi("biz/".$_POST['biz']);
+		}
+		if(move_uploaded_file($_FILES["file"]["tmp_name"],"biz/".$_POST['biz']."/" . $_FILES["file"]["name"])){
+			//##################################// uploaded/copied successfully
+			echo "ok";
+		}else{
+			echo "error";
+		}
 	}
-	move_uploaded_file($_FILES["file"]["tmp_name"],"biz/".$_POST['biz']."/" . $_FILES["file"]["name"]);
 ?>
