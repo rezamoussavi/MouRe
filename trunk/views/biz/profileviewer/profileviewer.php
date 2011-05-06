15a3370db8270b4bd4325c83aff3031158b076fb
rezamoussavi <reza2mussavi@hotmail.com>
2011-05-06 23:12:37 +0200

    profileviewer.biz updated

--- trunk/views/biz/profileviewer/profileviewer.php
+++ trunk/views/biz/profileviewer/profileviewer.php
@@ -4,10 +4,11 @@
 	Compiled by bizLang compiler version 4.0 [JQuery] (May 5 2011) By Reza Moussavi
 
 	Author:	Reza Moussavi
-	Date:	4/21/2011
-	Ver:		0.1
+	Date:	5/6/2011
+	Ver:		1.0
 
 */
+require_once 'biz/user/user.php';
 
 class profileviewer {
 
@@ -17,6 +18,7 @@ class profileviewer {
 	var $_tmpNode;
 
 	//Variables
+	var $message;
 
 	//Nodes (bizvars)
 
@@ -30,12 +32,18 @@ class profileviewer {
 		if(!isset($_SESSION['osNodes'][$fullname])){
 			$_SESSION['osNodes'][$fullname]=array();
 			//If any message need to be registered will placed here
+			$_SESSION['osMsg']['frame_updateInfo'][$this->_fullname]=true;
+			$_SESSION['osMsg']['frame_changePassword'][$this->_fullname]=true;
 		}
 
 		//default frame if exists
 		if(!isset($_SESSION['osNodes'][$fullname]['_curFrame']))
 			$_SESSION['osNodes'][$fullname]['_curFrame']='frm';
 		$this->_curFrame=&$_SESSION['osNodes'][$fullname]['_curFrame'];
+
+		if(!isset($_SESSION['osNodes'][$fullname]['message']))
+			$_SESSION['osNodes'][$fullname]['message']="";
+		$this->message=&$_SESSION['osNodes'][$fullname]['message'];
 
 		$_SESSION['osNodes'][$fullname]['node']=$this;
 		$_SESSION['osNodes'][$fullname]['biz']='profileviewer';
@@ -51,6 +59,12 @@ class profileviewer {
 
 	function message($message, $info) {
 		switch($message){
+			case 'frame_updateInfo':
+				$this->onUpdateInfo($info);
+				break;
+			case 'frame_changePassword':
+				$this->onChangePassword($info);
+				break;
 			default:
 				break;
 		}
@@ -94,14 +108,60 @@ JSONDOCREADY;
 //########################################
 
 
+	/*---------------------------------------------------------
+	-
+	------------------------------------------------------------*/
 	function frm(){
-		$html=<<<PHTMLCODE
+		$formName=$this->_fullname."ApplyBtn";
+		$formPass=$this->_fullname."Pass";
+		if(!osUserLogedin()){
+			$html="Please Login First";
+		}else{
+			$html=<<<PHTMLCODE
 
-			profile viewer
-		
+				{$this->message}
+				<div style="float:left;">
+					<form id="$formName" method="post" style="margin:10px;background-color:#FFCCFF;float:left;">
+						<input type="hidden" name="_message" value="frame_updateInfo" /><input type = "hidden" name="_target" value="{$this->_fullname}" />
+						Real Name: <input name="RealName" size=20><br>
+						Birth Date: <input name="BDate" size=20 ><br>
+						Address: <br><textarea name="Address" rows=4 cols=30></textarea><br>
+						Password: <input name="Password" type="password" size=20><br>
+						<div align=right><input type="button" value="Apply" onclick='Javascript:sndmsg("$formName")'></div>
+					</form>
+					<form id="$formPass" method="post" style="margin:10px;background-color:#CCFFFF;float:left;">
+						<input type="hidden" name="_message" value="frame_changePassword" /><input type = "hidden" name="_target" value="{$this->_fullname}" />
+						New Password: <input name="NewPass1" size=20 type="password"><br>
+						Confirm : <input name="NewPass2" size=20 type="password"><br>
+						Old Password: <input name="Password" type="password" size=20><br>
+						<div align=right><input type="button" value="Change Password" onclick='Javascript:sndmsg("$formPass")'></div>
+					</form>
+				</div>
+				<div style="float:left;margin:10px;background-color:#FFFFCC;">
+					Balance: <br>
+					Paid: <br>
+					Earned:
+				</div>
+			
 PHTMLCODE;
 
+		}
+		$this->message="";
 		return $html;
+	}
+	/*---------------------------------------------------------------
+	-		Message Handlers
+	---------------------------------------------------------------*/
+	function onChangePassword($info){
+		//check password match
+		if($info['NewPass1']!=$info['NewPass2']){
+			$this->message="<font color=red>Password and Confirm password dismatch! try again</font>";
+			return;
+		}
+		//applypass
+		//prepare error if any
+	}
+	function onUpdateInfo($info){
 	}
 
 }
